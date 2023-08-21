d636a6e4a4f8e72b39abed36c7cef8ef9a708b00
taenzeyang <tianze.yang@easystack.cn>
2023-08-21 14:15:54 +0800

    Support the build of arm architecture
    Signed-off-by: tianze.yang@easystack.cn <tianze.yang@easystack.cn>

--- build-img.sh
+++ build-img.sh
@@ -5,7 +5,8 @@ set -o nounset
 set -o xtrace
 
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd -P)
-IMAGE_FILE=${IMAGE_FILE:-"tkestack.io/gaia/vcuda:latest"}
+IMAGE_FILE=${IMAGE_FILE:-"hub.easystack.cn/production/vcuda"}
+IMAGE_TAG=${IMAGE_TAG:-"v1.0.6-es"}
 
 function cleanup() {
     rm -rf ${ROOT}/cuda-control.tar
@@ -16,6 +17,7 @@ trap cleanup EXIT SIGTERM SIGINT
 function build_img() {
     readonly local commit=$(git log --oneline | wc -l | sed -e 's,^[ \t]*,,')
     readonly local version=$(<"${ROOT}/VERSION")
+    readonly local arch=$(uname -m)
 
     rm -rf ${ROOT}/build
     mkdir ${ROOT}/build
@@ -24,7 +26,7 @@ function build_img() {
     cp ${ROOT}/Dockerfile ${ROOT}/build
     (
       cd ${ROOT}/build
-      docker build ${BUILD_FLAGS:-} --build-arg version=${version} --build-arg commit=${commit} -t ${IMAGE_FILE} .
+      docker build ${BUILD_FLAGS:-} --build-arg version=${version} --build-arg commit=${commit} --build-arg arch=${arch} -t ${IMAGE_FILE}:${IMAGE_TAG} .
     )
 }
 
